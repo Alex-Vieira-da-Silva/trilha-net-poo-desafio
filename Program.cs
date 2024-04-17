91d5fc3c034457b0e6e7c7a4c7333ea3dca14220
Alex-Vieira-da-Silva <alexvieira.360@hotmail.com>
2024-04-17 14:16:07 -0300

    commit finalizando o projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia= new Nokia("7305698", "Modelo 730", "3333655", 64);
+Console.WriteLine("Smartphone Nokia:");
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone("6985441", "Modelo 13", "1112336", 128);
+Console.WriteLine("Smartphone Iphone:");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
